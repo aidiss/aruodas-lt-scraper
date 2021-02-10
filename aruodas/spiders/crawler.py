6bc51a116d6db9411c2ee70616527f982b8a7ac9
Aidis Stukas <aidiss@GMAIL.COM>
2021-02-10 15:06:10 +0200

    Update crawler

--- aruodas/spiders/crawler.py
+++ aruodas/spiders/crawler.py
@@ -2,59 +2,61 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
+import logging
+import datetime
+
+DENY_LIST = ['dalyvio-taisykles', 'duk', 'nr1', 'nt_zemelapis', 'interjeras', 'straipsniai', 'kontaktai', r'sklypai/kaune/', 'imoniu-katalogas','informacija-perkantiems-busta', 'send-message', 'ideti-skelbima']
 
 
 class CrawlerSpider(CrawlSpider):
     name = 'crawler'
     allowed_domains = ['aruodas.lt']
-    start_urls = [
-        'https://www.aruodas.lt/butai',
-        'https://www.aruodas.lt/']
+    start_urls = ['https://www.aruodas.lt/butai', 'https://www.aruodas.lt/']
 
-    rules = (
-        Rule(
-            LinkExtractor(
-                allow=r'https://www.aruodas.lt/butai/puslapis/\d*?',
-                deny=['dalyvio-taisykles', 'duk', 'nr1',
-                      'nt_zemelapis', 'interjeras', 'straipsniai', 'kontaktai',
-                      r'sklypai/kaune/', 'imoniu-katalogas',
-                      'informacija-perkantiems-busta', 'send-message', 'ideti-skelbima']),
-            follow=True),
-        Rule(
-            LinkExtractor(
-                restrict_xpaths='//h3/a'),
-            callback='parse_item',
-            follow=True),
-    )
+    link_rule = Rule(LinkExtractor(allow=r'https://www.aruodas.lt/butai/puslapis/\d*?', deny=DENY_LIST), follow=True)
+    parse_rule = Rule(LinkExtractor(restrict_xpaths='//h3/a'), callback='parse_item', follow=True)
 
-    def parse_item(self, response):
+    rules = (link_rule, parse_rule)
+
+    def parse_item(self, r):
         i = {}
+        # Ordered by visual appearance.
+        i['href'] = r.url
+        i["breadcrumb"] = [x.strip() for x in r.css('div.obj-breadcrums>div>a>span::text').extract()]
+        i["header"] = r.css('h1.obj-header-text::text').extract_first().strip()
+        i['datetime'] = datetime.datetime.now()
+        i['summary'] = r.xpath('//*[@class="obj-summary"]/text()[2]').extract_first().strip()
+        total, today = r.css(".obj-top-stats>strong::text").extract_first().split("/")
+        i["views"] = {"total": total, "today": today}
 
-        i['href'] = response.url
+        # strip is risky
+        i["basket"] = r.css("span.control-number::text").extract_first()
 
-        r = response.css('dl.obj-details ::text').extract()
-        [x.strip() for x in r]
-        r = [x.strip() for x in r]
-        r = list(zip(r[1::5], r[3::5]))
-        r = [x for x in r if x[0] and x[1]]
-        r = dict(r)
-        i['info'] = r
+        i["price-eur"] = r.css("span.price-eur::text").extract_first().strip()
+        i["price-per"] = r.css("span.price-per::text").extract_first().strip()
 
-        i['comment'] = response.xpath(
-            '//p[@class="obj-comment"]//text()').extract()
+        # Details
+        data = [x.strip() for x in r.xpath("//dl[@class='obj-details ']/*[self::dt or self::dd]/text()").extract()]
+        details = list(zip(data[::2], data[1::2]))
+        details = dict(details)
 
-        i['atstumai'] = response.xpath(
-            '//div[@class="statistic-info-row"]/*//text()').extract()
+        details["Ypatybės:"] = r.xpath('//*[normalize-space(text()) = "Ypatybės:"]/following-sibling::*[1]/span/text()').extract()
+        details["Papildomos patalpos:"] = r.xpath('//*[normalize-space(text()) = "Papildomos patalpos:"]/following-sibling::*[1]/span/text()').extract()
+        details["Apsauga:"] = r.xpath('//*[normalize-space(text()) = "Apsauga:"]/following-sibling::*[1]/span/text()').extract()
+        i["details"] = details
 
-        i['summary'] = response.xpath(
-            '//*[@class="obj-summary"]/b/text()').extract()
+        i['description'] = r.xpath('//div[@class="obj-comment"]').extract_first()
 
-        informacija = response.css('div.obj-info-bg div dl *::text').extract()
+        # i['atstumai'] = r.xpath('//div[@class="statistic-info-row"]/*//text()').extract()
+
+        informacija = r.css('div.obj-info-bg div dl *::text').extract()
         informacija = informacija[1::2]
-        informacija = dict(zip(informacija[0::2], informacija[1::2]))
+        informacija = list(zip(informacija[0::2], informacija[1::2]))
+        informacija = {k: v for k, v in informacija if k and v}
         i['informacija'] = informacija
 
-        i['phone'] = response.css(
-            'div.phone.small::text').extract_first().strip()
-
+        # strip is risky
+        i['phone'] = r.css('div.phone>span::text').extract_first()
+        i["image"] = r.css('div.obj-img>img.obj-photo-big::attr(href)').extract_first()
+        i["images"] = r.css("div.obj-photos>a.link-obj-thumb::attr(href)").extract()
         return i
